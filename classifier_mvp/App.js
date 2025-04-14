7119efeda6dd9e527d6663f19da213e6e3498bfb
kartikey047 <ks129@illinois.edu>
2025-04-14 07:45:41 -0500

    Added POST Request from Front End JS

--- classifier_mvp/App.js
+++ classifier_mvp/App.js
@@ -1,19 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Button } from 'react-native';
 export default function App() {
+  const testPATH = 'C:\\Users\\kshar\\nature_notebook\\data\\005.Crested_Auklet\\Crested_Auklet_0018_1817.jpg';
+  const handlePredict = async () => {
+    const formData = new FormData();
+    formData.append('file', {
+      uri: testPATH,
+      name: 'test.jpg',
+      type: 'image/jpeg',
+    });
+    try {
+      const response = await fetch('http://127.0.0.1:5000/predict', {
+        method: 'POST',
+        headers: { 'Content-Type': 'multipart/form-data' },
+        body: formData,
+      });
+
+      const data = await response.json();
+      console.log('Prediction:', data);
+    } catch (error) {
+      console.error('Error while predicting:', error);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Button title="Send Image to /predict" onPress={handlePredict} />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
